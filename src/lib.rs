26055b1b4eb5b7473194f908bcb08b43e1a45da0
Colin Atkinson <colin.william.atkinson@gmail.com>
2015-06-24 00:51:58 -0400

    Change lib functions from operating on files to operating on byte arrays

--- src/lib.rs
+++ src/lib.rs
@@ -4,38 +4,55 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 use image::{GenericImage, Pixel};
+use image::png::PNGEncoder;
 
-pub fn encode_file(in_file: &str, out_file: &str) {
+pub fn write_bytes(buf: &[u8], out_file: &str) -> std::io::Result<()> {
+    let mut file = try!(File::create(&Path::new(out_file)));
+    try!(file.write_all(buf));
+
+    Ok(())
+}
+
+pub fn read_bytes(in_file: &str) -> std::io::Result<(Vec<u8>)> {
     let mut buf: Vec<u8> = Vec::new();
 
-    let mut file = File::open(&Path::new(in_file)).unwrap();
-    let result = file.read_to_end(&mut buf).unwrap() as u32;
-    let size = ((result as f64) / 4.0 + 1.0).sqrt().ceil() as u32;
+    let mut file = try!(File::open(&Path::new(in_file)));
+    try!(file.read_to_end(&mut buf));
 
-    buf.insert(0, ((result >> 24) & 0xFF) as u8);
-    buf.insert(1, ((result >> 16) & 0xFF) as u8);
-    buf.insert(2, ((result >>  8) & 0xFF) as u8);
-    buf.insert(3, ((result >>  0) & 0xFF) as u8);
+    Ok(buf)
+}
 
-    println!("{:?}", (size.pow(2) * 4 - (buf.len() as u32)).to_string().as_bytes());
+pub fn encode_file(in_buf: &[u8]) -> Result<Vec<u8>, image::ImageError> {
+    let mut buf: Vec<u8> = in_buf.to_vec();
 
-    println!("{} {}", buf.len(), size.pow(2) * 4);
+    let len = buf.len() as u32;
+    let img_size = ((len as f64) / 4.0 + 1.0).sqrt().ceil() as u32;
 
-    while (buf.len() as u32) < size.pow(2) * 4 {
+    buf.insert(0, ((len >> 24) & 0xFF) as u8);
+    buf.insert(1, ((len >> 16) & 0xFF) as u8);
+    buf.insert(2, ((len >>  8) & 0xFF) as u8);
+    buf.insert(3, ((len >>  0) & 0xFF) as u8);
+
+    while (buf.len() as u32) < img_size.pow(2) * 4 {
         buf.push(0 as u8);
     }
 
-    image::save_buffer(&Path::new(out_file), &buf, size, size, image::RGBA(8));
+    let mut out_buf: Vec<u8> = Vec::new();
+
+    {
+        let mut enc = PNGEncoder::new(&mut out_buf);
+        try!(enc.encode(&buf, img_size, img_size, image::RGBA(8)));
+    }
+
+    Ok(out_buf)
 }
 
-pub fn decode_file(in_file: &str, out_file: &str) {
-    println!("Opening image");
-    let img = image::open(&Path::new(in_file)).unwrap();
-    println!("Image opened");
+pub fn decode_file(in_buf: &[u8]) -> Result<Vec<u8>, image::ImageError> {
+    let img = try!(image::load_from_memory_with_format(in_buf, image::ImageFormat::PNG));
 
     let mut buf: Vec<u8> = Vec::new();
 
-    for (x, y, pixel) in img.pixels() {
+    for (_, _, pixel) in img.pixels() {
         for i in pixel.channels() {
             buf.push(*i);
         }
@@ -44,10 +61,9 @@ pub fn decode_file(in_file: &str, out_file: &str) {
     let size: usize = (((buf[0] as u32) << 24)
                      | ((buf[1] as u32) << 16)
                      | ((buf[2] as u32) <<  8)
-                     | ((buf[3] as u32) <<  0) as usize;
+                     | ((buf[3] as u32) <<  0)) as usize;
 
-    println!("{}", size);
+    let ret: Vec<u8> = (&buf[4..(size + 4)]).to_vec();
 
-    let mut file = File::create(&Path::new(out_file)).unwrap();
-    file.write_all(&buf[4..(size + 4)]);
+    Ok(ret)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::io::prelude::*;
-use std::fs::File;
-
 extern crate image_hider;
 extern crate image;
 
 fn main() {
     println!("Encoding file");
-    image_hider::encode_file("/home/colin/Videos/TV/Freaks and Geeks/Season 1/FreaksAndGeeks - s01e01.mkv", "output.png");
+    let res = image_hider::encode_file(&image_hider::read_bytes("contact.mp3").unwrap()).unwrap();
+    image_hider::write_bytes(&res, "output2.png").unwrap();
 
     println!("Decoding file");
-    image_hider::decode_file("output.png", "freaks.mkv");
+    let res = image_hider::decode_file(&image_hider::read_bytes("output2.png").unwrap()).unwrap();
+    image_hider::write_bytes(&res, "contact2.mp3").unwrap();
 }
